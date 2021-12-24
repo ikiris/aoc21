dad89924335ae8ddb77dc7ea4b34c44126b2b40a
ikiris <4852950+ikiris@users.noreply.github.com>
2021-12-24 04:25:54 -0600

    d1 complete

--- d1/main.go
+++ d1/main.go
@@ -2,32 +2,16 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"io"
-	"log"
-	"os"
 	"strconv"
 
 	"github.com/pkg/errors"
 )
 
-func main() {
-	fh, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatalf("failed to open: %v", err)
-	}
-
-	c, err := doThings(fh)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(c)
-}
-
-func doThings(r io.Reader) (int64, error) {
+func p1(r io.Reader) (int64, error) {
 	s := bufio.NewScanner(r)
-	var last int64
 	var c int64
+	var last int64
 	for s.Scan() {
 		i, err := strconv.ParseInt(s.Text(), 10, 64)
 		if err != nil {
@@ -38,5 +22,25 @@ func doThings(r io.Reader) (int64, error) {
 		}
 		last = i
 	}
-	return c, nil
+	return c - 1, nil
+}
+
+func p2(r io.Reader) (int64, error) {
+	s := bufio.NewScanner(r)
+	var c int64
+	var a, b int64
+	var sum int64
+	for s.Scan() {
+		i, err := strconv.ParseInt(s.Text(), 10, 64)
+		if err != nil {
+			return 0, errors.Wrapf(err, "failed to parse %v:", s.Text())
+		}
+		newsum := a + b + i
+		a, b = b, i
+		if newsum > sum {
+			c++
+		}
+		sum = newsum
+	}
+	return c - 3, nil
 }
