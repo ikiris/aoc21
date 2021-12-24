5f3362c32f9e3751a7b84e52b007e199f209d53a
ikiris <4852950+ikiris@users.noreply.github.com>
2021-12-24 05:08:43 -0600

    d2 p2 complete

--- d2/main.go
+++ d2/main.go
@@ -32,3 +32,29 @@ func p1(r io.Reader) (int64, error) {
 	}
 	return depth * x, nil
 }
+
+func p2(r io.Reader) (int64, error) {
+	var x, depth, aim int64
+	dir := map[string]func(int64){
+		"forward": func(i int64) {
+			x += i
+			depth += i * aim
+		},
+		"up":   func(i int64) { aim -= i },
+		"down": func(i int64) { aim += i },
+	}
+
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		d, v, ok := strings.Cut(s.Text(), " ")
+		if !ok {
+			return 0, fmt.Errorf("malformed input string: %s", s.Text())
+		}
+		i, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			return 0, errors.Wrapf(err, "failed to parse %v:", s.Text())
+		}
+		dir[d](i)
+	}
+	return depth * x, nil
+}
--- d2/main_test.go
+++ d2/main_test.go
@@ -46,3 +46,34 @@ func TestP1(t *testing.T) {
 		})
 	}
 }
+func TestP2(t *testing.T) {
+	tests := []struct {
+		name    string
+		data    io.Reader
+		want    int64
+		wantErr bool
+	}{
+		{
+			"basic",
+			getHandle(t, "testdata/input.txt"),
+			900,
+			false,
+		}, {
+			"aocd2",
+			getHandle(t, "testdata/input2.txt"),
+			0,
+			false,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := p2(tc.data)
+			if (err != nil) != tc.wantErr {
+				t.Errorf("func %s goterr: %v wanted: %v", tc.name, err, tc.wantErr)
+			}
+			if got != tc.want {
+				t.Errorf("func got: %d, want: %d", got, tc.want)
+			}
+		})
+	}
+}
